aaf5323178c6fe20c78818ed540de6b67173027b
Yuliya Raikova <yuradev2001@gmail.com>
2022-06-29 22:23:04 +0300

    .

--- src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
+++ src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
@@ -2,6 +2,7 @@ package school.mjc.stage0.loops.task1;
 
 public class RepeatIsBad {
     public void repeatIsBad(){
-
+        String message = "writing the same code doesn't have much impact, and it's also time consuming";
+        System.out.println(message.repeat(10));
     }
 }
